04f063b7f387dff15c18d66d115d17fb7270e1d4
Harshgupta182 <61505754+Harshgupta182@users.noreply.github.com>
2021-12-07 11:35:17 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,185 @@
+(function($) {
+  "use strict"
+
+
+// TIMER
+
+// Set the date we're counting down to
+var countDownDate = new Date("23 oct 2021 , 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timer1").innerHTML =  hours + "hour ";
+  
+  document.getElementById("timer2").innerHTML =  
+   minutes + "min " ;
+  document.getElementById("timer3").innerHTML =  
+  seconds + "sec ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+  // NAVIGATION
+  var responsiveNav = $('#responsive-nav'),
+    catToggle = $('#responsive-nav .category-nav .category-header'),
+    catList = $('#responsive-nav .category-nav .category-list'),
+    menuToggle = $('#responsive-nav .menu-nav .menu-header'),
+    menuList = $('#responsive-nav .menu-nav .menu-list');
+
+  catToggle.on('click', function() {
+    menuList.removeClass('open');
+    catList.toggleClass('open');
+  });
+
+  menuToggle.on('click', function() {
+    catList.removeClass('open');
+    menuList.toggleClass('open');
+  });
+
+  $(document).click(function(event) {
+    if (!$(event.target).closest(responsiveNav).length) {
+      if (responsiveNav.hasClass('open')) {
+        responsiveNav.removeClass('open');
+        $('#navigation').removeClass('shadow');
+      } else {
+        if ($(event.target).closest('.nav-toggle > button').length) {
+          if (!menuList.hasClass('open') && !catList.hasClass('open')) {
+            menuList.addClass('open');
+          }
+          $('#navigation').addClass('shadow');
+          responsiveNav.addClass('open');
+        }
+      }
+    }
+  });
+
+  // HOME SLICK
+  $('#home-slick').slick({
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    arrows: true,
+  });
+
+  // PRODUCTS SLICK
+  $('#product-slick-1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+    appendDots: '.product-slick-dots-1',
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: false,
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('#product-slick-2').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+    appendDots: '.product-slick-dots-2',
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: false,
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  // PRODUCT DETAILS SLICK
+  $('#product-main-view').slick({
+    infinite: true,
+    speed: 300,
+    dots: false,
+    arrows: true,
+    fade: true,
+    asNavFor: '#product-view',
+  });
+
+  $('#product-view').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    focusOnSelect: true,
+    asNavFor: '#product-main-view',
+  });
+
+  // PRODUCT ZOOM
+  $('#product-main-view .product-view').zoom();
+
+  // PRICE SLIDER
+  var slider = document.getElementById('price-slider');
+  if (slider) {
+    noUiSlider.create(slider, {
+      start: [1, 999],
+      connect: true,
+      tooltips: [true, true],
+      format: {
+        to: function(value) {
+          return value.toFixed(2) + '$';
+        },
+        from: function(value) {
+          return value
+        }
+      },
+      range: {
+        'min': 1,
+        'max': 999
+      }
+    });
+  }
+
+})(jQuery);
